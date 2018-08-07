5ae89c17e9feb2630e6cb3651a5b563121b0e47b
wandererparsifal <wandererparsifal@gmail.com>
2018-08-07 17:49:03 +0800

    添加 RandomWords

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,11 @@
-import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
+import 'package:flutter/material.dart';
 
 void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final wordPair = new WordPair.random();
     return new MaterialApp(
       title: 'Welcome to Flutter',
       home: new Scaffold(
@@ -15,9 +14,22 @@ class MyApp extends StatelessWidget {
         ),
         body: new Center(
           //child: new Text('Hello World'),
-          child: new Text(wordPair.asPascalCase),
+          child: new RandomWords(),
         ),
       ),
     );
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  @override
+  createState() => new RandomWordsState();
+}
+
+class RandomWordsState extends State<RandomWords> {
+  @override
+  Widget build(BuildContext context) {
+    final wordPair = new WordPair.random();
+    return new Text(wordPair.asPascalCase);
   }
 }
